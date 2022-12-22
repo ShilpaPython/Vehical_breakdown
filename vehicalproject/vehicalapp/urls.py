06039c246fedb2fe81f15f13432279aa69be7888
ShilpaPython <117644865+ShilpaManish@users.noreply.github.com>
2022-12-22 18:16:03 +0530

    shilpa

--- vehicalproject/vehicalapp/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('',views.index,name="index"),
-    path('custsignup',views.customersignup,name="custsignup"),
-    path('customersignin',views.customersignin,name="customersignin"),
-]
